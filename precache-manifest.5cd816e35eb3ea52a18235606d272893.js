68d8de39c91f989ae4a5889d0e284b59f5eea0c0
abelMarkos <abelmarkos2020@gmail.com>
2019-05-03 04:16:06 -1200

    Updates

---
+++ precache-manifest.5cd816e35eb3ea52a18235606d272893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bcf07a78b4e329816bb",
+    "url": "/recipes-for-you/static/js/runtime~main.6bcf07a7.js"
+  },
+  {
+    "revision": "7593690a9dfd0594ce44",
+    "url": "/recipes-for-you/static/js/main.7593690a.chunk.js"
+  },
+  {
+    "revision": "8edd912e12e2a356ba88",
+    "url": "/recipes-for-you/static/js/1.8edd912e.chunk.js"
+  },
+  {
+    "revision": "7593690a9dfd0594ce44",
+    "url": "/recipes-for-you/static/css/main.25b5d452.chunk.css"
+  },
+  {
+    "revision": "8edd912e12e2a356ba88",
+    "url": "/recipes-for-you/static/css/1.db5bb447.chunk.css"
+  },
+  {
+    "revision": "083af5706a65ac6affc5ee3c17fe1460",
+    "url": "/recipes-for-you/index.html"
+  }
+];
